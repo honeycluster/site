28f517c8042fa92c86de4d85b9bc7a1b7e6aa5e8
interc0der <intercoder@whirled.io>
2024-05-01 21:55:45 -0500

    Update: revise meta tags

--- src/components/general/meta/index.tsx
+++ src/components/general/meta/index.tsx
@@ -1,41 +1,32 @@
 import React from 'react';
 import Head from 'next/head';
 
-const SITE_ROOT = 'https://' + (process.env.VERCEL_URL || 'concept.aquaduck.xyz');
+const SITE_ROOT = 'https://' + (process.env.VERCEL_URL || 'site.honcluster.dev');
 
 const Meta = () => {
   return (
     <Head>
       {/* 
         <!-- HTML Meta Tags --> */}
-      <title>AQUADUCK</title>
-      <meta
-        name="description"
-        content="A x-chain bridge platform supporting the XRP Ledger and the EVM Sidechain"
-      />
+      <title>Honeycluster</title>
+      <meta name="description" content="The Hive of the XRP Ledger." />
       <meta name="msapplication-TileColor" content="#000000" />
 
       {/*         <!-- Facebook Meta Tags --> */}
       <meta property="og:url" content={`${SITE_ROOT}`} />
       <meta property="og:type" content="website" />
-      <meta property="og:title" content="AQUADUCK" />
-      <meta
-        property="og:description"
-        content="A x-chain bridge platform supporting the XRP Ledger and the EVM Sidechain"
-      />
+      <meta property="og:title" content="Honeycluster" />
+      <meta property="og:description" content="The Hive of the XRP Ledger." />
       <meta property="og:image" content={`${SITE_ROOT}/og-image.png`} />
 
       {/*         <!-- Twitter Meta Tags --> */}
-      <meta name="twitter:site" content="@aquaduckxyz" />
-      <meta name="twitter:creator" content="@aquaduckxyz" />
+      <meta name="twitter:site" content="@HoneyclusterX" />
+      <meta name="twitter:creator" content="@HoneyclusterX" />
       <meta name="twitter:card" content="summary_large_image" />
       <meta property="twitter:domain" content={`${SITE_ROOT}`} />
       <meta property="twitter:url" content={`${SITE_ROOT}`} />
-      <meta name="twitter:title" content="AQUADUCK" />
-      <meta
-        name="twitter:description"
-        content="A x-chain bridge platform supporting the XRP Ledger and the EVM Sidechain"
-      />
+      <meta name="twitter:title" content="Honeycluster" />
+      <meta name="twitter:description" content="The Hive of the XRP Ledger." />
       <meta name="twitter:image" content={`${SITE_ROOT}/og-image.png`} />
     </Head>
   );
